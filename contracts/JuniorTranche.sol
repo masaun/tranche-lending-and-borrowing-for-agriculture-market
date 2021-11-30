3337af5eaaf47a9e6aaf8564924ffd6e995d8bb9
masaun <masanoriuno@gmail.com>
2021-11-30 19:41:49 +0900

    [SC]: Inherit the JuniorTrancheToken.sol into the TranchePool.sol

--- contracts/JuniorTranche.sol
+++ contracts/JuniorTranche.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.7.6;
 
 
 /**
- * @dev - This is a pool for the Junior Tranche
+ * @dev - This is a pool (Bond) contract for the Junior Tranche
  */ 
 contract JuniorTranche {
 
--- contracts/TranchePool.sol
+++ contracts/TranchePool.sol
@@ -1,16 +1,27 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.7.6;
 
+import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import { JuniorTrancheToken } from "./JuniorTrancheToken.sol";
+
 
 /**
  * @dev - This is a smart contract that manage pools of tranches (Pool of Senior Tranche / Junior Tranche)
- *        (Reference from the SmartYield.sol in the BarnBridge)
+ *        (Same role with the SmartYield.sol in the BarnBridge)
  * 
  * @dev - This smart contract is integrated with existing Lending Protocols in order to generate yield for allocating into 2 Pools (Senior/Junior).
  */ 
 contract TranchePool {
 
-    constructor() {}
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint8 decimals_
+    ) JuniorTrancheToken(name_, symbol_, decimals_) {
+        // [Todo]:
+    }
 
     /**
      * @dev - Deposit stablecoins (e.g. DAI, USDC, USDT) into existing lending protocols (e.g. AAVE, Compound)
