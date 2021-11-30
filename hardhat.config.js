81a6a41252a17fb778fb52f9a02c2147fcd8e36e
masaun <masanoriuno@gmail.com>
2021-11-30 17:33:26 +0900

    [hardhat.coonfig.js]: Add solc-compiler v0.7.6

--- hardhat.config.js
+++ hardhat.config.js
@@ -49,9 +49,9 @@ module.exports = {
 
   solidity: {
     compilers: [
-      // {
-      //   version: '0.6.11'
-      // },
+      {
+        version: '0.7.6'
+      },
       {
         version: '0.8.0'
       }
