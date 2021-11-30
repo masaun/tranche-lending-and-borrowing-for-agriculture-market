d0555de384b2f449d1205607ae2727e39a0314ef
masaun <masanoriuno@gmail.com>
2021-11-30 14:06:15 +0900

    [SC]: Remove console.log()

--- contracts/TranchePool.sol
+++ contracts/TranchePool.sol
@@ -1,8 +1,6 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-//import "hardhat/console.sol";
-
 
 /**
  * @dev - This is a smart contract that manage pools of tranches (Pool of Senior Tranche / Junior Tranche)
@@ -12,10 +10,7 @@ pragma solidity ^0.8.0;
  */ 
 contract TranchePool {
 
-    constructor(string memory _testMessage) {
-        console.log("Test: ", _testMessage);
-    }
-
+    constructor() {}
 
     /**
      * @dev - Deposit stablecoins (e.g. DAI, USDC, USDT) into existing lending protocols (e.g. AAVE, Compound)
