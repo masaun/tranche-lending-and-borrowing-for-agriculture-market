b847f3bc27b133ee5bc402c29b458cff3c3c0dc4
masaun <masanoriuno@gmail.com>
2021-12-09 10:53:05 -0500

    [Test]: Define the scenario.test.js as a test file for whole senario (from tranche lending to borrowing)

---
+++ test/scenario.test.js
@@ -0,0 +1,8 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Scenario test (Tranche lending ~ borrowing)", function () {
+    it("Something", async function () {
+        // [TODO]:
+    })
+})
