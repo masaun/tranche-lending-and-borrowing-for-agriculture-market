efa0a18f4db858a70a430e076dbd595908118b93
masaun <masanoriuno@gmail.com>
2021-12-09 13:12:36 -0500

    [Test]: Execute currentBlock() in scenario.test.js

--- test/scenario.test.js
+++ test/scenario.test.js
@@ -2,7 +2,17 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Scenario test (Tranche lending ~ borrowing)", function () {
-    it("Something", async function () {
+
+    async function currentBlock() {
+        return await ethers.provider.getBlock('latest')
+    }
+
+    it("Check currentBlock", async function () {
         // [TODO]:
+        const _currentBlock = await currentBlock()
+        console.log(`currentBlock: ${ JSON.stringify(_currentBlock) }`)
     })
+
+
+
 })
